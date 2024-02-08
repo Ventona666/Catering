2a5240dfef50d7bbff64ca4d42e97e9012e98009
Tony De Freitas <defreitas.tony.pro@gmail.com>
2024-02-08 10:38:54 +0100

    [CAT-16] Début page d'accueil

--- app/src/main/java/com/example/catering/MainActivity.java
+++ app/src/main/java/com/example/catering/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.catering;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listItems;
+    private ArrayList<String> listRestaurants = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page);
+        listItems = this.findViewById(R.id.list_item);
+
+        listRestaurants.add("Rest 1");
+        listRestaurants.add("Rest 2");
+        listRestaurants.add("Rest 3");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listRestaurants);
+        listItems.setAdapter(adapter);
     }
 }
